8c9d877082348fd978feaf1d2be69cfc0988f0f3
alicia487 <twd5741@gmail.com>
2025-08-29 16:20:03 +0900

    update day17

--- Day17/quiz-game-start/main.py
+++ Day17/quiz-game-start/main.py
@@ -4,11 +4,12 @@ from data import question_data
 
 question = Question()
 
-def question_bank(self):
+def question_bank(self, q_text, q_answer):
     questions = []
-    for q_data in self.question:
-        question_text = q_data["text"]
-        question_answer = q_data["answer"]
-    return questions
+    for data in question_data:
+        question_text = self.q_text["text"]
+        question_answer = self.q_answer["answer"]
 
 
+
+question_bank(data,"text","answer")
